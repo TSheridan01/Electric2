dc9efbe85a1ae69994084023628cee1ded1d7bc6
Megha Jain <meghajain.1984@gmail.com>
2023-01-09 00:17:21 -0500

    added models based on per capita

--- ML_join_queries.sql
+++ ML_join_queries.sql
@@ -18,9 +18,25 @@ FROM HOUSEHOLD_INCOME A,
 WHERE A.STATECODE_OF_COUNTY = B.STATECODE_OF_COUNTY
 	AND A.COUNTY = B.COUNTY;
 
+--Income vs EV count per county - Capita_income_yr_2021_ev_count.csv - linear_regression_perCap_Inc21_ev_cnt.ipynb
+SELECT A.STATECODE_OF_COUNTY,
+	A.COUNTY,
+	A.per_capita_income_2021,
+	B.EV_COUNT
+FROM HOUSEHOLD_INCOME A,
+
+	(SELECT county_fips,STATECODE_OF_COUNTY,
+			COUNTY,
+			COUNT(VEHICLE_NAME) AS EV_COUNT
+		FROM EV_REGISTRATION
+		GROUP BY county_fips, STATECODE_OF_COUNTY,
+			COUNTY) B
+WHERE A.county_fips = B.county_fips;
 
 
---income vs EV count and stations count per county - counts_per_county.csv - linear_regression_ev_count_station_cnt.ipynb, unsupervised_K_income_ev_stations_cnt.ipynb(no PCA used)
+
+
+--income vs EV count and stations count per county - counts_per_county.csv - linear_regression_ev_count_station_cnt.ipynb,multiple_regression_income_ev_cnt_station_cnt, unsupervised_K_income_ev_stations_cnt.ipynb(no PCA used)
 SELECT A.STATECODE_OF_COUNTY,
 	A.COUNTY,
 	A.INCOME_YR_2021,
@@ -49,7 +65,33 @@ WHERE A.STATECODE_OF_COUNTY = B.STATECODE_OF_COUNTY
 	AND B.COUNTY = C.COUNTY
 	order by vehicle_cnt desc;
 
---income vs stations count per county - income_all_yrs_station_cnt.csv -unsupervised_K_income_stations_cnt.ipynb, unsupervised_hierarchical_income_station_cnt.ipynb
+--income vs EV count and stations count per county - counts_per_county_capita21.csv - multiple_regression_perCapita21_inc_ev_cnt_station_cnt.ipynb,unsupervised_K_cap_Inc21_ev_stations_cnt.ipynb(no PCA used)
+SELECT A.STATECODE_OF_COUNTY,
+	A.COUNTY,
+	A.per_capita_income_2021,
+	B.VEHICLE_CNT,
+	C.STATION_CNT
+FROM HOUSEHOLD_INCOME A,
+
+	(SELECT STATECODE_OF_COUNTY,
+			COUNTY,
+			COUNT(VEHICLE_NAME) VEHICLE_CNT
+		FROM EV_REGISTRATION
+		GROUP BY STATECODE_OF_COUNTY,
+			COUNTY) B,
+
+	(SELECT STATECODE_OF_COUNTY,
+			COUNTY,
+			COUNT(FUEL_TYPE_CODE) STATION_CNT
+		FROM ALTERNATIVE_FUELING_STATION
+		GROUP BY STATECODE_OF_COUNTY,
+			COUNTY) C
+WHERE A.STATECODE_OF_COUNTY = B.STATECODE_OF_COUNTY
+	AND A.COUNTY = B.COUNTY
+	AND B.STATECODE_OF_COUNTY = C.STATECODE_OF_COUNTY
+	AND B.COUNTY = C.COUNTY;
+
+--income vs stations count per county - income_all_yrs_station_cnt.csv -linear_regression_income_station_cnt.ipynb, unsupervised_K_income_stations_cnt.ipynb, unsupervised_hierarchical_income_station_cnt.ipynb
 SELECT A.INCOME_YR_2019, A.INCOME_YR_2020, A.INCOME_YR_2021,
 	B.STATIONS_COUNT
 FROM HOUSEHOLD_INCOME A,
@@ -61,10 +103,16 @@ FROM HOUSEHOLD_INCOME A,
 			COUNTY) B
 WHERE A.STATECODE_OF_COUNTY = B.STATECODE_OF_COUNTY
 	AND A.COUNTY = B.COUNTY;
-	
--- select a.statecode_of_county, a.county, a.income_yr_2021, b.fuel_type_code, c.vehicle_name
--- from household_income a, alternative_fueling_station b, ev_registration c
--- where a.statecode_of_county = b.statecode_of_county
--- and a.county = b.county
--- and b.statecode_of_county = c.statecode_of_county
--- and b.county = c.county;
+
+--income vs stations count per county - income_perCapita21_station_cnt.csv -linear_regression_perCap21_inc_station_cnt.ipynb
+SELECT A.per_capita_income_2021,
+	B.STATIONS_COUNT
+FROM HOUSEHOLD_INCOME A,
+	(SELECT STATECODE_OF_COUNTY,
+			COUNTY,
+			COUNT(FUEL_TYPE_CODE) AS STATIONS_COUNT
+		FROM ALTERNATIVE_FUELING_STATION
+		GROUP BY STATECODE_OF_COUNTY,
+			COUNTY) B
+WHERE A.STATECODE_OF_COUNTY = B.STATECODE_OF_COUNTY
+	AND A.COUNTY = B.COUNTY;
